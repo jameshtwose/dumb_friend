b3c2298fbc5c57b05af709d81b98ceb7010f38c4
James Twose <jamestwose@Mac-mini.home>
2022-07-18 19:52:40 +0200

    The conversational AI works, but it is way too big for the free deta micro

--- chatbot_eda/EDA.py
+++ chatbot_eda/EDA.py
@@ -2,16 +2,26 @@
 import chatterbot
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import pymongo
+from dotenv import load_dotenv, find_dotenv
+import os
+# %%
+_ = load_dotenv(find_dotenv())
+user=os.environ["user"]
+pwd=os.environ["pwd"]
+mongo_db_string=f"mongodb+srv://{user}:{pwd}@cluster0.yf82syg.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(mongo_db_string)
+db = client.test
+# %%
+db
+
 # %%
 # Create a chatbot instance and name it
 chatbot = ChatBot('dumb friend', 
-                #   logic_adapters=[
-        # {
-        #     "import_path": "chatterbot.logic.BestMatch",
-        #     "statement_comparison_function": chatterbot.comparisons.LevenshteinDistance,
-        #     "response_selection_method": chatterbot.response_selection.get_first_response
-        # }]
-                  )
+                  storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+                #   logic_adapters=['chatterbot.logic.BestMatch'],
+                database="cluster0",
+                  database_uri=mongo_db_string)
 
 #%%
 # Create a new trainer for the chatbot
@@ -22,4 +32,5 @@ trainer.train("chatterbot.corpus.english")
 #%%
 # Get a response to an input statement
 chatbot.get_response("Hello, how are you today?").text
+
 # %%
